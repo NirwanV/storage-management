dc595f4af1aa8805b11047c889ad7c2d34115dbc
NirwanV <vimukthajith@gmail.com>
2025-09-02 01:02:42 +0530

    Auth env devlopment started

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="center">
+      <h1 className="h1">StoreIt-The only storage you need</h1>
+    </div>
+  );
+}
